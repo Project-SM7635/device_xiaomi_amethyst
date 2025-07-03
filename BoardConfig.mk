789a975219be6cd483caaa3c284277c93bef304f
AdarshGrewal <adarshgrewal@gmail.com>
2025-07-03 16:17:00 +0530

    amethyst: Switch to libperfmgr
    Change-Id: I25a11fa51b72a9752f07b1aa217b15c7d84a8a99
    Signed-off-by: zerotwo-alt <amruthv780@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -189,6 +189,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2026-01-01
 
 # Sepolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
